d2dbcb072313fd7f1070ae670ae81b29cef74c4a
vasu-tamrakar <vasu.tamrakar@cssindiaonline.com>
2020-08-04 13:03:47 +0530

    updated

--- main/App.php
+++ main/App.php
@@ -6,8 +6,16 @@ class App{
         return array('first_name'=>$this->first_name, 'last_name'=>$this->last_name);
     }
     public function setUser($first,$last){
-        $this->first_name= $first;
-        $this->last_name=$last;
+        if(isset($first)){
+            $this->first_name=$first;
+        }else{
+            $this->first_name='';
+        }
+        if(isset($last)){
+            $this->last_name=$last;
+        }else{
+            $this->last_name='';
+        }
         return true;
     }
 }
